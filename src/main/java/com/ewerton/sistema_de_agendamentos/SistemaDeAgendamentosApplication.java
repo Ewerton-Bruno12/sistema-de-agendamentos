06207503a923adfe5555e00f78e8aff9390d7aac
Ewerton Bruno <ewerton12042006@gmail.com>
2026-09-17 20:13:21 -0300

    Primeiro commit do projeto

---
+++ src/main/java/com/ewerton/sistema_de_agendamentos/SistemaDeAgendamentosApplication.java
@@ -0,0 +1,13 @@
+package com.ewerton.sistema_de_agendamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeAgendamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeAgendamentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ewerton/sistema_de_agendamentos/SistemaDeAgendamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ewerton.sistema_de_agendamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeAgendamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
