30af161f312c6d874e90cb9e78238cfb7f5ac4e1
Ewerton Bruno <ewerton12042006@gmail.com>
2026-09-17 20:40:13 -0300

    feat: cria ServiceEntity

---
+++ src/main/java/com/ewerton/sistema_de_agendamentos/model/ServiceEntity.java
@@ -0,0 +1,31 @@
+package com.ewerton.sistema_de_agendamentos.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "tb_service")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ServiceEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    private String description;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @Column(name = "duration_minutes", nullable = false)
+    private Integer durationMinutes;
+}
