2f41dfee79464887dd01189ef9722135cdd83089
Ewerton Bruno <ewerton12042006@gmail.com>
2026-09-17 20:33:35 -0300

    feat: cria ClientEntity

--- src/main/java/com/ewerton/sistema_de_agendamentos/SistemaDeAgendamentosApplication.java
+++ src/main/java/com/ewerton/sistema_de_agendamentos/SistemaDeAgendamentosApplication.java
@@ -2,8 +2,10 @@ package com.ewerton.sistema_de_agendamentos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SistemaDeAgendamentosApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/ewerton/sistema_de_agendamentos/model/ClientEntity.java
@@ -0,0 +1,37 @@
+package com.ewerton.sistema_de_agendamentos.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tb_client")
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ClientEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+}
