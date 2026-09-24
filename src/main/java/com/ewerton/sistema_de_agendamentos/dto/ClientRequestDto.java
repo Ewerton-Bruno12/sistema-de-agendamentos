ea248e49dc0f5435a4bcbabeff24cb238627b9f2
Ewerton Bruno <ewerton12042006@gmail.com>
2026-09-24 19:17:50 -0300

    feat: adiciona validacao do formato do telefone

--- src/main/java/com/ewerton/sistema_de_agendamentos/dto/ClientRequestDto.java
+++ src/main/java/com/ewerton/sistema_de_agendamentos/dto/ClientRequestDto.java
@@ -2,6 +2,7 @@ package com.ewerton.sistema_de_agendamentos.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 public record ClientRequestDto(
         @NotBlank(message = "O nome do cliente é obrigatório")
@@ -10,5 +11,9 @@ public record ClientRequestDto(
         @Email(message = "E-mail inválido")
         String email,
         @NotBlank(message = "O telefone é obrigatório")
+        @Pattern(
+                regexp = "^\\d{10,11}$",
+                message = "Telefone inválido. Use apenas números com DDD (10 ou 11 dígitos)"
+        )
         String phone
 ) {}
